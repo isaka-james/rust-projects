f2c4afceb4f71882aff79c3bd7d7bbb0b5886e74
isaka-james <mr.isakajames@gmail.com>
2024-10-26 23:57:55 +0300

    inner and outer - also some loops

--- 1. Beginners/6. Comprehesive-Rust/main.rs
+++ 1. Beginners/6. Comprehesive-Rust/main.rs
@@ -10,11 +10,31 @@ fn rust_default_types(){
     println!("An error has been simulated, because of the equivalenting different variable types!");
 }
 
-fn finabocci_series(var_x: u32)->u32{
-    if var_x <= 2 {
-        return var_x;
-    }else{
-        return var_x + finabocci_series(var_x-1);
+fn finabocci_series(n: u32)->u32{
+    if n <= 2 {
+        return n
+    }
+
+    return n + finabocci_series(n-1);
+}
+
+fn outer_and_inner(){
+    // This checks the first student to have the 
+    let student_marks = [ [60,62,34],[56,45,70],[100,67,23],[34,34,12]];
+    'outer: for i in 0..3{
+        println!("This is the {i} student marking!");
+        'inner: for j in 0..3{
+            if student_marks[i][j] == 100 {
+                println!("Got a student who hit: marks={}, Stopping marking rightaway",student_marks[i][j]);
+                break 'outer;
+            }else if student_marks[i][j] >= 50 {
+                println!("Student {i} is doing good: marks={}!",student_marks[i][j]);
+            }else if student_marks[i][j] < 50 {
+                println!("This student is doing very bad, Stopping accessing his other marks. his marks={}!",student_marks[i][j]);
+                break 'inner;
+            }
+        }
+        println!("Going for another student.");
     }
 }
 
@@ -24,4 +44,7 @@ fn main(){
 
    let number_for_finabocci:u32 = 20; 
    println!("The finabocci series for 20 is: {}",finabocci_series(number_for_finabocci));
+
+   // Using outer and inner
+   outer_and_inner();
 }
